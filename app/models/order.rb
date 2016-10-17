09bcbf2b2d70b62eff4f17caa9d1f822056450c6
jessiewuwu <jwu004@gmail.com>
2016-10-17 07:08:33 -0700

    Add comments on how to handle orders that need to get fulfilled at different warehouses

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,38 +7,22 @@ class Order < ActiveRecord::Base
   end
 
   def find_warehouse
-    # what happens if the order has to be shipped between different warehouses?
     
-    warehouse_id = Inventory.where('twin >= ? AND twinXL >= ? AND full >= ? AND queen >= ? AND king >= ? AND calking >=?', self.twin, self.twinXL, self.full, self.queen, self.king, self.calking ).first.warehouse_id
+    list_of_possible_inventories = Inventory.where('twin >= ? AND twinXL >= ? AND full >= ? AND queen >= ? AND king >= ? AND calking >=?', self.twin, self.twinXL, self.full, self.queen, self.king, self.calking )
 
-    send_order(warehouse_id, self.id)
-  end
+    if list_of_possible_inventories.any? 
+      warehouse_id = list_of_possible_inventories.first.warehouse_id
+      send_order(warehouse_id, self.id)
+    else # what happens if the order has to be shipped between different warehouses?
+      list_of_inventories = []
+      order_list.each_with_index do |(k,v), index|
+        str = "#{k.to_s} >= ?"
+        list_of_inventories.push(Inventory.where(str, v))
+      end
 
-  def find_warehouse2(order_list)
-   #order_list = {twin: 1, calking: 1}
-   # reformat the keys so that you can do a query
-   # plug into Inventory.where(str1 AND str2, val1, val2)
-
-   # if there is a warehouse that has everything, send the order there
-   #Inventory.where(str1 AND str2, v1,v2)
-   query = ""
-   order_list.each_with_index do |(k,v), index|
-    if index == 0
-      query += "#{k.to_s} >= ?"
-    else
-      query += " AND #{k.to_s} >= ?" 
+      # for duplicates, meaning the same warehouse has 2 of the product sizes requested from order, send 1 order there with the appropriate product-qty
+      # for non-duplicates, send 1 order there with the appropriate product-qyt
     end
-    #list_of_inventories.push(Inventory.where(str, v))
-   end
-   warehouse_id = Inventory.where(query, order_list.map{|k,v| v.to_i }.join(", ") ).first.warehouse_id
-
-   # else - 
-
-   list_of_inventories = []
-   order_list.each_with_index do |(k,v), index|
-    str = "#{k.to_s} >= ?"
-    list_of_inventories.push(Inventory.where(str, v))
-   end
 
   end
 
